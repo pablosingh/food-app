14098e3cef593ae2b084ceb2ef075f6406ae1302
Singh Pablo <pablo22_4@hotmail.com>
2022-09-01 12:38:59 -0300

    post en firestore

--- src/components/TestFirebase.jsx
+++ src/components/TestFirebase.jsx
@@ -1,20 +1,33 @@
 
 import { useEffect } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 import db from '../firebase/firebaseConfig';
 
 export const TestFirebase = () => {
+    const getData = async() => {
+        try {
+            const data = await getDocs( collection( db, 'dinners' ) );
+            data.forEach( doc => {
+                console.log(doc.data());
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    const postData = async() => {
+        try {
+            const docRef = await addDoc( collection( db, 'dinners' ), {
+                nombre: 'Manuel',
+                edad: '33'
+            } );
+            console.log( docRef );
+            console.log( docRef.id );
+        } catch (error) {   
+            console.error(error);
+        }
+    };
     useEffect( () => {
-        const getData = async() => {
-            try {
-                const data = await getDocs( collection( db, 'dinners' ) );
-                data.forEach( doc => {
-                    console.log(doc.data());
-                });
-            } catch (error) {
-                console.error(error);
-            }
-        };
+        // postData();
         getData();
     },[]);
     return (
